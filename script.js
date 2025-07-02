584510895d39f6e76a7ef9924163f2190175770e
sulyman002 <oyedelesulaiman@gmail.com>
2025-07-02 20:42:01 +0100

    done with the js

--- script.js
+++ script.js
@@ -2,15 +2,20 @@
 const submt = document.querySelector('.submit'); //submit button
 const inputBox = document.querySelector('.inputBox'); //input box
 const errorContent = document.querySelector('.errorContent');
+const put = document.querySelector('.put');
 
 let isError = false;
 
-submt.addEventListener('click', () =>{
+submt.addEventListener('click', (preventDefault) => {
+    preventDefault.preventDefault();
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if(!emailRegex.test(inputBox.value.trim())){
+
+    if (!emailRegex.test(inputBox.value.trim())) {
         errorContent.classList.remove('hidden');
+        inputBox.style.background = 'rgba(255, 98, 87, 0.27)'
         isError = true;
     }
+
 
 
 });
