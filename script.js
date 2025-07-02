f7e312758044bdbe3e6a0e34f62e55cbf6633a25
sulyman002 <oyedelesulaiman@gmail.com>
2025-07-02 22:24:12 +0100

    done with the project

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //get all element needed
 const submt = document.querySelector('.submit'); //submit button
 const inputBox = document.querySelector('.inputBox'); //input box
-const errorContent = document.querySelector('.errorContent');
-const put = document.querySelector('.put');
+const errorContent = document.querySelector('.errorContent');//error
+const container = document.querySelector('.container'); //container
+const dismiss = document.querySelector('.dismiss'); //dismiss button
+const dismissContent = document.querySelector('.dismissContent'); //dismiss content
 
 let isError = false;
 
@@ -14,8 +16,20 @@ submt.addEventListener('click', (preventDefault) => {
         errorContent.classList.remove('hidden');
         inputBox.style.background = 'rgba(255, 98, 87, 0.27)'
         isError = true;
+    }else{
+        errorContent.classList.add('hidden');
+        inputBox.style.background = 'rgba(36, 39, 66, 0.1)';
+        isError = false;
+        container.classList.add('hidden');
+        container.classList.remove('md:flex');
+        dismissContent.classList.remove('hidden');
     }
+});
 
-
+dismiss.addEventListener('click', () => {
+    dismissContent.classList.add('hidden');
+    container.classList.remove('hidden');
+     container.classList.add('md:flex');
+     location.reload();
 
 });
